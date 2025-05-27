89921e9bf37c90c49a9a66e169b4d835db576bed
FredyDsd <fredymauro513@gmail.com>
2025-05-27 11:51:12 -0300

    feat: se agrego el arreglo principal para guardar los objetos alumnos

--- trabajo-practico-5/src/App.jsx
+++ trabajo-practico-5/src/App.jsx
@@ -2,8 +2,12 @@
 import "./App.css";
 import { MainLayout } from "./layouts/MainLayout";
 import { HomePage } from "./pages/homePage/HomePage";
+import { useState } from "react";
 
 function App() {
+
+  const [alumnos, setAlumnos] = useState([{ Lu: 'APU00999', nombre: "Maria Eugenia", apellido: "Diaz", curso: 'Tercero', email: 'mariadiaz@gmail.com', domicilio: 'Av. Congreso 125', telefono: '3884895999' }]);
+  const [alumno, setAlumno] = useState({});
 
   return (
     <MainLayout>
